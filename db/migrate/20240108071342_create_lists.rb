dce2e0587cec04926dcd9a28198399a1e0e9e64c
nagataku26 <nagataku.biz@gmail.com>
2024-01-08 16:22:34 +0900

    renderへのデプロイ準備のためのトップページ作成

---
+++ db/migrate/20240108071342_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+      t.string :selected_icon, null: false
+      t.string :list_name, null: false
+      t.string :list_password, null: false
+      t.timestamps
+    end
+  end
+end
