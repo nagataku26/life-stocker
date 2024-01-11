c2e633f4fb20e2f39ac4b05c350f08cc43a72a16
nagataku26 <nagataku.biz@gmail.com>
2024-01-11 18:07:57 +0900

    リスト作成ページの作成

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+
+
+  ICONS = %w(icon1.png icon2.png icon3.png icon4.png icon5.png icon6.png icon7.png icon8.png icon9.png icon10.png icon11.png icon12.png) # 使用可能なアイコンのリスト
+
+  validates :selected_icon, inclusion: { in: ICONS }, allow_nil: true
 end
