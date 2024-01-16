56ce8c375dab0734665b2fc40c3b62abf859af64
nagataku26 <nagataku.biz@gmail.com>
2024-01-16 23:27:20 +0900

    デザイン編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'lists/index'
   root to: "lists#index"
   resources :users, only: :show
-  resources :lists, only: [:index, :new, :create, :show, :edit] do
+  resources :lists, only: [:index, :new, :create, :show, :edit, :destroy] do
     resources :items, only: [:new, :create]
   end
 end
