74a4a67106a7eba8a1ed87f4ee3b7c217eaf2006
nagataku26 <nagataku.biz@gmail.com>
2024-01-17 14:45:08 +0900

    防災グッズリスト、買い物リストの初期作成の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-   has_many :user_lists, dependent: :destroy
-   has_many :lists, through: :user_lists
-   has_many :items, through: :lists
+  has_many :user_lists, dependent: :destroy
+  has_many :lists, through: :user_lists
+  has_many :items, through: :lists
 
-   validates :nickname, presence: true
+  validates :nickname, presence: true
+
+  after_create :create_initial_lists
+
+  private
+  def create_initial_lists
+    disaster_prevention_list = lists.create(list_name: "防災グッズリスト", list_password: "0000")
+    purchase_list = lists.create(list_name: "買い物予定リスト", list_password: "0000")
+  end
 end
