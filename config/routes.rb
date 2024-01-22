ea36ee5000b8cbfdd8fa078fecd8c6d1f8605923
nagataku26 <nagataku.biz@gmail.com>
2024-01-22 14:53:58 +0900

    購入予定ページデザイン編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'lists/index'
   root to: "lists#index"
-  resources :users, only: :show
+  resources :users do
+    member do
+      get "all_items"
+      get "purchase_plan"
+    end
+  end
+
   resources :lists, only: [:index, :new, :create, :show, :edit, :destroy] do
     resources :items, only: [:new, :create]
   end
