22302f7b446a0874bbf725baf74337115e43e41f
nagataku26 <nagataku.biz@gmail.com>
2024-02-03 23:37:03 +0900

    アイテム詳細画面へのリンク設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :item_find, only: [:show]
 
   def new
     @list = List.find(params[:list_id])
@@ -24,8 +23,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :stock_count, :stock_place, :purchase_id, :purchase_date, :expiration_id, :expiration_date, :purchase_plan, :purchase_plan_count, :url, :memo, :image).merge(list_id: params[:list_id])
   end
-
-  def item_find
-    @item = Item.find(params[:item_id])
-  end
+  
 end
