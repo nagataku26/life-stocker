b35370a7875f6f826259333dcba673de8a51b13a
nagataku26 <nagataku.biz@gmail.com>
2024-02-13 11:03:25 +0900

    バリデーション編集

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,10 +5,12 @@ class List < ApplicationRecord
   has_many :items, dependent: :destroy
 
     validates :list_name, presence: true
-    validates :shares_id, length: {in: 4..10 },
-                             format: { with: /[a-z\d]/i, message: 'は4桁以上10桁以内の半角英数字で入力してください'}
+    validates :shared_id, length: {in: 4..10 },
+                             format: { with: /[a-z\d]/i, message: 'は4桁以上10桁以内の半角英数字で入力してください'},
+                             allow_blank: true
     validates :shared_password, length: { is: 4 },
-                             format: { with: /\A\d{4}\z/, message: 'は4桁の半角数字で入力してください' }
+                             format: { with: /\A\d{4}\z/, message: 'は4桁の半角数字で入力してください' },
+                             allow_blank: true
 
   def show
     @user = current_user
