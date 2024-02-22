79916b3da29f550a56d2b809fcfc4dcb12f8019c
nagataku26 <nagataku.biz@gmail.com>
2024-02-22 12:44:33 +0900

    リスト登録後の遷移ページ変更

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to root_path
+      redirect_to @list
     else
       render :new, status: :unprocessable_entity
     end
